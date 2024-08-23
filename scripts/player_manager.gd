ccb1488fc90852223036eb19ce4ee8abf65d1b3d
Artur Skarżyński <artur.skarzynski@getindata.com>
2024-08-24 00:31:59 +0200

    ognisko i update player managera

--- scripts/player_manager.gd
+++ scripts/player_manager.gd
@@ -1,14 +1,18 @@
 extends Node
 
-var hp = 10
+var max_hp = 10
+var hp = 5
 var money = 0
 var level = 1
 var exp = 0
-var immunity = 0
+var max_immunity: float = 2.0
+var immunity: float = 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if immunity > 0.0:
+		immunity = max(immunity-delta, 0)
+	
 	
 func add_exp(amount: int) -> void:
 	exp += amount
@@ -18,7 +22,17 @@ func add_exp(amount: int) -> void:
 	print("Level up! Your level is now %s" % level)
 	
 func deal_damage(amount: int) -> void:
+	if immunity > 0:
+		return
 	hp -= amount
 	if hp < 0:
 		print("YOU DIED")
 		get_tree().reload_current_scene()
+	print("Dostałes hita za %s" % amount)
+	print("Masz teraz %s hp" % hp)
+	immunity = max_immunity
+
+func heal(amount: int) -> void:
+	hp += amount
+	print("healed")
+	hp = min(hp, max_hp)
