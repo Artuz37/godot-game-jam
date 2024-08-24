d145af2f05757a388a0fc73816a307c7754920d5
Lisci4asty <igorskarzynski3@gmail.com>
2024-08-24 19:35:34 +0200

    rollowanie i fukcja grant_immunity

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var roll_timer = $Timer
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var rolling = false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -26,3 +28,16 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	
+	if Input.is_action_just_pressed("roll") and not rolling:
+		PlayerManager.grant_immunity(0.5)
+		animated_sprite.play("roll")
+		roll_timer.start()
+		rolling = true
+		
+
+
+func _on_timer_timeout():
+	rolling = false
+	animated_sprite.play("idle")
